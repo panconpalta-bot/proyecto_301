a4946fe91f9fdb4b78c907dce918b52b858bdd21
jack lozano <1634309@senati.pe>
2025-10-29 11:02:37 -0500

    feat:Creando configuracion inicial de rect

---
+++ src/frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <div className="min-h-screen bg-grey-100 p-8">
+            <h1 className="text-2xl font-bold mb-4">BIENVENIDO A MI PAGINA</h1>
+            <p className="text-orange-600 text-xl">esta es nuestra aplicacion</p>
+        </div>
+    );
+}
+
+export default Home;
